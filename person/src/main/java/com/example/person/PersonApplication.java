9ccb7e3017859465a064161430cc6f42d9998da8
Анастасия <hurtovanasta85@gmail.com>
2026-02-04 09:25:10 +0300

    upd

--- person/src/main/java/com/example/person/PersonApplication.java
+++ person/src/main/java/com/example/person/PersonApplication.java
@@ -1,13 +1,34 @@
 package com.example.person;
 
+import com.example.person.model.Person;
+import com.example.person.repository.PersonRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PersonApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PersonApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initPersons(PersonRepository repository) {
+		return args -> {
+
+			// очищаем, чтобы при каждом старте не дублировались данные
+			repository.deleteAll();
+
+			repository.save(new Person(
+					"Ingeborga",
+					"Riga"
+			));
+
+			repository.save(new Person(
+					"Natalia",
+					"Voronezh"
+			));
+		};
+	}
 }
